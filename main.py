d98c14cfa7583402cf12549ffb5c59739f093e2b
GYosifov88 <gyosifov88@gmail.com>
2026-06-30 15:38:31 +0300

    environment setup

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+
+def main():
+    print("Hello from langchain-course!")
+
+
+if __name__ == "__main__":
+    main()
