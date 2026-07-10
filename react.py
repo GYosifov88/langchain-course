a3573d2b845cc39ae93989b3f4f18002f7dda7d0
GYosifov88 <gyosifov88@gmail.com>
2026-07-10 11:25:34 +0300

    function calling reasoning

---
+++ react.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_openai import OpenAI
+from langchain_tavily import TavilySearch
+
+load_dotenv()  # Load environment variables from .env file
+
+@tool
+def triple(num: float) -> float:
+    """
+    param num: A number to be tripled.
+    return: The tripled value of the input number.
+    """
+    return float(num) * 3
+
+tools = [TavilySearch(max_results=1), triple]
+
+
+
