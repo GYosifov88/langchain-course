1cc6f61e2aa0bdc2226a27a95dbb5d50c7774659
GYosifov88 <gyosifov88@gmail.com>
2026-08-03 15:38:43 +0300

    adding initial logic in main for mcp client

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import asyncio
+
 from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
 
 load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["D:\\langchain-course\\servers\\math_server.py"],
+)
 
 async def main():
     print("Hello from langchain-course!")
